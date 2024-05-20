2f49319cd3335761d313b0a2238f769c0303077a
MirkoValle <mirko.valle20@gmail.com>
2024-05-20 20:24:09 +0200

    Added points counter and winning situoation. also added output text for points results

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 const btn = document.querySelector("button");
 const gridEl = document.getElementById("grid");
 const bombsN = 16;
+const score = document.querySelector("h2");
+const esito = document.querySelector("h2.esito");
 
 btn.addEventListener("click", function () {
     gridEl.innerHTML = ""
+    score.innerHTML = ""
+    esito.innerHTML = ""
 
     let points = 0;
 
-    gridGenerator(points);
+    gridGenerator(points, bombsN, score, esito);
 
 
 });
 
-
-function gridGenerator(points) {
+function gridGenerator(points, bombsN, score, esito) {
 
     // Cambio il numero di celle in base alla modalità scelta
     let cellN;
@@ -30,7 +33,6 @@ function gridGenerator(points) {
     // Genero l'array con l'indice delle bombe
     let arrayBombs = bombsGenerator(cellN, bombsN);
 
-
     for (let index = 0; index < cellN; index++) {
         const cellEl = document.createElement("article");
 
@@ -38,33 +40,47 @@ function gridGenerator(points) {
             cellEl.classList.add("bomb")
             let bombImg = document.createElement("span");
             cellEl.appendChild(bombImg);
+            cellEl.append(index + 1);
         }
         cellEl.classList.add("cell", mode);
 
+
         cellEl.addEventListener("click", function () {
+
 
             if (cellEl.classList.contains("clicked") == false) {
                 cellEl.classList.add("active")
                 cellEl.classList.add("clicked")
+                if (cellEl.classList.contains("bomb")) {
+                    stop(score, points)
+                    esito.innerHTML = "Hai perso, ritenta sarai più fortunato"
+                }
+
+                points += 1;
+                console.log(points)
+
+                let allCell = document.getElementsByClassName("cell")
+                if (points === allCell.length - bombsN) {
+                    stop(score, points)
+                    esito.innerHTML = "Congratulazioni hai vinto!!"
+                }
             }
-
-            if (cellEl.classList.contains("bomb")) {
-                stop()
-            }
-
-
         });
 
         gridEl.appendChild(cellEl);
     }
 }
 
-function stop() {
+
+
+function stop(score, points) {
     let allCell = document.getElementsByClassName("cell")
 
     for (let index = 0; index < allCell.length; index++) {
         allCell[index].classList.add("clicked");
     }
+    score.innerHTML = "Punteggio finale: " + points
+
 }
 
 function getRandomNumber(min, max) {
