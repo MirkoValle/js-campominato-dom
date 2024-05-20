08301acef24315a953bc20499a412f7c87a70946
MirkoValle <mirko.valle20@gmail.com>
2024-05-20 19:46:50 +0200

    Added functions for: adding bombs inside the grill, check if the cell it's been already clicked and make it unclickable, add a stop function to make all the cell unclickable and stop the game

--- js/script.js
+++ js/script.js
@@ -5,42 +5,67 @@ const bombsN = 16;
 btn.addEventListener("click", function () {
     gridEl.innerHTML = ""
 
-    gridGenerator();
+    let points = 0;
+
+    gridGenerator(points);
 
 
 });
 
 
-function gridGenerator() {
+function gridGenerator(points) {
 
+    // Cambio il numero di celle in base alla modalità scelta
     let cellN;
     let modeEl = document.getElementById("mode");
     let mode = modeEl.value;
 
     if (mode === "easy") {
-        cellN = 49;
+        cellN = 100;
     } else if (mode === "medium") {
         cellN = 81;
     } else {
-        cellN = 100;
+        cellN = 49;
     }
-
+    // Genero l'array con l'indice delle bombe
     let arrayBombs = bombsGenerator(cellN, bombsN);
-    console.log(arrayBombs)
+
 
     for (let index = 0; index < cellN; index++) {
         const cellEl = document.createElement("article");
+
+        if (arrayBombs.includes(index)) {
+            cellEl.classList.add("bomb")
+            let bombImg = document.createElement("span");
+            cellEl.appendChild(bombImg);
+        }
         cellEl.classList.add("cell", mode);
-        cellEl.append(index + 1);
+
         cellEl.addEventListener("click", function () {
-            cellEl.classList.toggle("active")
-            console.log(index + 1)
+
+            if (cellEl.classList.contains("clicked") == false) {
+                cellEl.classList.add("active")
+                cellEl.classList.add("clicked")
+            }
+
+            if (cellEl.classList.contains("bomb")) {
+                stop()
+            }
+
+
         });
 
         gridEl.appendChild(cellEl);
     }
 }
 
+function stop() {
+    let allCell = document.getElementsByClassName("cell")
+
+    for (let index = 0; index < allCell.length; index++) {
+        allCell[index].classList.add("clicked");
+    }
+}
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min)) + min;
